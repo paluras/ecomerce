dee4849ba254b1becd3e56cb7726372c299ce13f
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-21 14:44:16 +0200

    fix imgs

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -8,13 +8,13 @@ function Nav({count , handleSidebar, cart ,handleCart, cartSide,cartItems} : {ca
         <nav >
           
           <div className="left-nav">
-            <img className='menu' onClick={handleSidebar} src="src\assets\icon-menu.svg" alt="" />
-            <img className='logo' src='src\assets\logo.svg'></img>
+            <img className='menu' onClick={handleSidebar} src="https://raw.githubusercontent.com/paluras/ecomerce/91ac1df4f44d4aa40c57ab3bc8328b8611aca607/src/assets/icon-menu.svg" alt="" />
+            <img className='logo' src='https://raw.githubusercontent.com/paluras/ecomerce/91ac1df4f44d4aa40c57ab3bc8328b8611aca607/src/assets/logo.svg'></img>
             <div style={{width:count}} className="left-nav-items">
               <ul>
                 <img onClick={handleSidebar} 
                 className='close' 
-                src="src\assets\icon-close.svg" 
+                src="https://raw.githubusercontent.com/paluras/ecomerce/91ac1df4f44d4aa40c57ab3bc8328b8611aca607/src/assets/icon-close.svg" 
                 alt="icon-close" />
                 <li><Link to="/">Collection</Link></li>
                 <li><Link to="/men">Men</Link></li>
@@ -26,13 +26,13 @@ function Nav({count , handleSidebar, cart ,handleCart, cartSide,cartItems} : {ca
             </div>
             <div className="right-nav">
               <div className="uno">
-              <img  onClick ={handleCart} src="src\assets\icon-cart.svg" alt="" />
+              <img  onClick ={handleCart} src="https://raw.githubusercontent.com/paluras/ecomerce/91ac1df4f44d4aa40c57ab3bc8328b8611aca607/src/assets/icon-cart.svg" alt="" />
               <div className="cart-items">{cartItems.length}</div>
               </div>
               <div style={{width:cartSide}} className="cart">
                 {cart}
               </div>
-              <img className='profile' src="src\assets\image-avatar.png" alt="" />
+              <img className='profile' src="https://github.com/paluras/ecomerce/blob/master/src/assets/image-avatar.png?raw=true" alt="" />
             </div>
           
             
